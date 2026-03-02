e42cc18df2d3af9145073e0148989089c78e22d9
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-03-01 18:12:23 -0800

    Shuffled stuff around, made it EJS compatible

--- app.js
+++ app.js
@@ -5,17 +5,55 @@ import express from 'express';
 const app = express();
 
 // Define a port number where server will listen
-const PORT = 3000;
+const PORT = 3012;
 
 // Enable static file serving
 app.use(express.static('public'));
+
+// Set EJS as view engine
+app.set('view engine', 'ejs');
+
+// "Middleware" that lets express read form data and store in req.body
+app.use(express.urlencoded ({ extended: true }));
+
+// Create temp array to store entries
+const entries = [];
 
 // Define a default "route" ('/')
 // req: contains information about the incoming request
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/form.html`);
+    res.render(`home`);
 });
+
+app.get('/guestbook', (req, res) => {
+    res.render(`form`);
+});
+
+// Confirmation route
+app.post('/guestbook', (req, res) => {
+    const entry = {
+        first_name: req.body['first-name'],
+        last_name: req.body['last-name'],
+        job_title: req.body['job-title'],
+        company: req.body.company,
+        linkedin: req.body.linkedin,
+        email: req.body.email,
+        meet: req.body.meet,
+        other: req.body.other,
+        comments: req.body.comments,
+        timestamp: new Date()
+    };
+
+    entries.push(entry);
+
+    res.render(`confirmation`, { entry });
+});
+
+// Admin route
+app.get('/admin', (req, res) => {
+    res.render('admin', { entries });
+})
 
 // Start server and listen on the specified port
 app.listen(PORT, () => {
